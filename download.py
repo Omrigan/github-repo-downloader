e2f4694538853ffa723fc439b8d6fb7c72a110bc
Omrigan <omrigann@gmail.com>
2018-02-10 21:55:26 +0300

    Semaphore

--- download.py
+++ download.py
@@ -1,14 +1,16 @@
-import git, plumbum
+import git
 import requests
-import os, asyncio, time
+import os, time
 from os import path as osp
 import threading
 import shutil
+import argparse
 
 latency = 7
 cwd = os.getcwd()
 perpage = 100
 
+semaphore = threading.Semaphore(20)
 downloaded = open("downloaded.txt", "a")
 
 
@@ -22,6 +24,7 @@ class MyThread(threading.Thread):
         user = self.repo["owner"]["login"]
         local_fullname = r"%s@%s" % (user, name)
         path = osp.join(osp.join(cwd, 'data'), local_fullname)
+        semaphore.acquire()
         if osp.exists(path):
             print("Removing %s" % local_fullname)
             shutil.rmtree(path)
@@ -29,17 +32,19 @@ class MyThread(threading.Thread):
         git.Repo.clone_from(self.repo["html_url"], path, )
         downloaded.write("%s\n" % self.repo["full_name"])
         downloaded.flush()
+        semaphore.release()
         print("Done %s" % local_fullname)
 
 downloaded_set = set()
 scheduled_set = set()
 if osp.exists("downloaded.txt"):
-    downloaded_set = set(open("downloaded.txt").readlines())
+    downloaded_set = set([str(line).strip() for line in open("downloaded.txt").readlines()])
     print("Preloaded %s repos" % len(downloaded_set))
 if not osp.exists("data"):
     os.mkdir("data")
 threads = []
 for page in range(100):
+    semaphore.acquire()
     result = requests.get("https://api.github.com/search/repositories", params=
     dict(sort="stars", order="desc", q="language:python", perpage=perpage, page=page)).json()
     if 'items' in result:
@@ -48,14 +53,23 @@ for page in range(100):
         print("Wait a bit")
 
     for repo in result["items"]:
-        if repo["full_name"] not in downloaded_set and repo["full_name"] not in scheduled_set:
+
+        if repo["full_name"]  in downloaded_set:
+            print("Already exists %s" % repo["full_name"])
+        elif repo["full_name"]  in scheduled_set:
+            print("Already scheduled %s" % repo["full_name"])
+        else:
             t = MyThread(repo)
             t.start()
             threads.append(t)
             scheduled_set.add(repo["full_name"])
-        else:
-            print("Already exists %s" % repo["full_name"])
+
+
+    semaphore.release()
     time.sleep(latency)
 
 for t in threads:
     t.join()
+
+
+
