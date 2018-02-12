4c859ffee996455ec03c25d3744fbfa2d482f3b8
lubimovnik <lubimovnik@yandex-team.ru>
2018-02-12 11:34:06 +0300

    some model improvements

--- find_main_contributor.py
+++ find_main_contributor.py
@@ -5,15 +5,15 @@ import codecs
 import json
 
 from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.neighbors import KNeighborsClassifier
 from sklearn.preprocessing import LabelEncoder
+from sklearn.linear_model import LogisticRegression
 
 from download import DATA_DIR
 
 
 class FindMainContributor(object):
 
-    def __init__(self, vocabulary_size=100):
+    def __init__(self, vocabulary_size=10000):
         main_contributors, content = [], []
         for root, _, files in os.walk(DATA_DIR):
             for filename in files:
@@ -23,9 +23,9 @@ class FindMainContributor(object):
                         main_contributors.append(self._main_contributor_string(filename, data))
                         content.append(data['readme_content'])
 
-        self._preprocessor = TfidfVectorizer(max_features=vocabulary_size)
+        self._preprocessor = TfidfVectorizer(max_df=0.9, max_features=vocabulary_size)
         self._label_encoder = LabelEncoder()
-        self._model = KNeighborsClassifier(n_neighbors=1)
+        self._model = LogisticRegression()
         self._model.fit(
             X=self._preprocessor.fit_transform(content),
             y=self._label_encoder.fit_transform(main_contributors)
@@ -55,5 +55,6 @@ def prompt(find_main_contributor):
 if __name__ == "__main__":
     print('Inititalize model...')
     find_main_contributor = FindMainContributor()
+    print find_main_contributor._preprocessor.vocabulary_
     print('Type your query:')
     prompt(find_main_contributor)
